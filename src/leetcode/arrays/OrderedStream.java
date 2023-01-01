5863665b7eaaee9706130c5075484bba9a3eeca0
Bheem Negi <bheem.negi3@gmail.com>
2023-01-01 10:41:21 +0530

    leetcode challenges: https://leetcode.com/problems/design-an-ordered-stream

--- src/leetcode/arrays/OrderedStream.java
+++ src/leetcode/arrays/OrderedStream.java
@@ -1,9 +1,41 @@
 package leetcode.arrays;
 
-import kotlin.collections.ArrayDeque;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class OrderedStream {
+class OrderedStream {
+
+    String[] orderedValues;
+    int index = 0;
+    int len = 0;
+
+    public OrderedStream(int n) {
+        orderedValues = new String[n + 1];
+        len = orderedValues.length;
+    }
+
+    public List<String> insert(int idKey, String value) {
+        List<String> newList = new ArrayList<>();
+        orderedValues[idKey - 1] = value;
+        if (index < len  && orderedValues[index] != null) {
+            while (orderedValues[index] != null) {
+                newList.add(orderedValues[index]);
+                index++;
+            }
+        }
+        return newList;
+    }
+
+    public static void main(String[] args) {
+        OrderedStream orderedStream = new OrderedStream(5);
+        System.out.println(orderedStream.insert(3, "ccccc"));
+        System.out.println(orderedStream.insert(1, "aaaaa"));
+        System.out.println(orderedStream.insert(2, "bbbbb"));
+        System.out.println(orderedStream.insert(5, "eeeee"));
+        System.out.println(orderedStream.insert(4, "ddddd"));
+    }
+
 
 }
+
