a7376051b0bdefaa234c43d8e1460e3b0c097b40
Bheem Negi <bheem.negi3@gmail.com>
2023-01-14 09:41:19 +0530

    leetcode challenges: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/

---
+++ src/leetcode/arrays/SpecialDiscount.java
@@ -0,0 +1,34 @@
+package leetcode.arrays;
+
+public class SpecialDiscount {
+
+    public static int[] finalPrices(int[] prices) {
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] >= prices[j]) {
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+
+    public static int[] finalPrices2(int[] prices) {
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] >= prices[j]) {
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+
+    public static void main(String[] args) {
+        int []arr = new int[]{8,4,6,2,3};
+        System.out.println(finalPrices(arr));
+    }
+
+}
